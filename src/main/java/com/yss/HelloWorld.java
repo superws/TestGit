8ad8c11af1652b6792671b2b6f19166ce8c84975
wusong <w1149138661@qq.com>
2018-06-01 10:47:06 +0800

    修改稿

--- src/main/java/com/yss/HelloWorld.java
+++ src/main/java/com/yss/HelloWorld.java
@@ -3,7 +3,7 @@ package com.yss;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hello world2!");
         System.out.println("hello");
+
     }
 }
